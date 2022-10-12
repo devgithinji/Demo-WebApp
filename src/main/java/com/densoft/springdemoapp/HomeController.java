cca3721e6b56a279e7d5b0798e8d44ecd5e0d74a
dennis githinji <wakahiad@gmail.com>
2022-10-12 18:10:56 +0300

    object fetching and hibernate logs fix

--- src/main/java/com/densoft/springdemoapp/HomeController.java
+++ src/main/java/com/densoft/springdemoapp/HomeController.java
@@ -7,6 +7,8 @@ import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
@@ -22,7 +24,7 @@ public class HomeController {
 
         try {
             //create 3 student objects
-            Student student = new Student("dennis", "githinji", "dennis@gmail.com");
+            Student student = new Student("paul", "kamau", "paul@gmail.com");
             //start transaction
             session.beginTransaction();
 
@@ -36,9 +38,9 @@ public class HomeController {
             session = factory.getCurrentSession();
             session.beginTransaction();
             // retrieve student based on the id: primary key
-            System.out.println("Saved student. Generated Id. "+student.getId());
+            System.out.println("Saved student. Generated Id. " + student.getId());
             Student myStudent = session.get(Student.class, student.getId());
-            System.out.println("Get complete student: "+myStudent);
+            System.out.println("Get complete student: " + myStudent);
             //commit transaction
             session.getTransaction().commit();
 
@@ -50,6 +52,44 @@ public class HomeController {
         }
 
         return "main-menu";
+    }
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+            // retrieve student based on the id: primary key
+            System.out.println("Retrieve students");
+            //query all students
+            List<Student> students = session.createQuery("from Student ").list();
+
+
+            students = session.createQuery("from Student s where s.lastName = 'doe'").list();
+
+            students = session.createQuery("from Student s where s.lastName = 'doe' or s.firstName = 'test'").list();
+
+            students = session.createQuery("from Student s where s.email LIKE '%nis@gmail.com'").list();
+
+            for (Student student : students) {
+                System.out.println(student);
+            }
+
+            //commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("done");
+
+        } catch (Exception e) {
+            System.out.println(e);
+            factory.close();
+        }
+
     }
 
 
